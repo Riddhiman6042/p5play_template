e5ea76c0a1b1dd7807c357fe712250743a58aa45
Riddhiman6042 <88177803+Riddhiman6042@users.noreply.github.com>
2021-08-07 05:29:04 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,29 @@
-
 function setup() {
-  createCanvas(400,400);
+  createCanvas(1900,950);
 }
 
 function draw() 
 {
-  background(30);
+  background(255,255,255);
 
+  if(keyIsDown(UP_ARROW))
+   {
+     background(255,154,0);
+   }
+
+   if(keyIsDown(DOWN_ARROW))
+    {
+      background(0,154,255);
+    }
+
+    if(keyIsDown(LEFT_ARROW))
+     {
+       background(255,0,154);
+     }
+     if(keyIsDown(RIGHT_ARROW))
+      {
+        background(50,0,255);
+      }
 }
 
 
